8b456610439933851114bf0eb6f08b3958e2f4a3
Your Name <you@example.com>
2019-06-14 00:48:10 -0400

    first commit

---
+++ Web Scrapping.py
@@ -0,0 +1,31 @@
+﻿importrequests, json, re
+
+
+html = requests.get('https://www.daraz.pk/catalog/?q=Bridal%20dress').text
+
+
+jsonStr = re.search(r'window.pageData=(.*?)</script>', html).group(1)
+jsonObject = json.loads(jsonStr)
+
+
+
+
+filename = "Bridal Dress.csv"
+f = open(filename, "w")
+
+
+headers = "product_name, price\n"
+f.write(headers)
+
+
+
+
+
+
+for item in jsonObject['mods']['listItems']:
+         print(item['name'])
+         print(item['price'])
+         f.write(item['name'].replace(",","") +","+ item['price'].replace(",","") + "\n")
+
+
+f.close()
